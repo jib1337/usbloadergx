177ce2e0f2ead0b9f8fc2e71b6503d05cf898470
Jack Nelson <38575814+jib1337@users.noreply.github.com>
2025-07-09 18:53:57 +1000

    Stop DVD drive on menu

--- source/menu.cpp
+++ source/menu.cpp
@@ -166,6 +166,8 @@ int MainMenu(int menu)
 {
 	currentMenu = menu;
 
+	InitNetworkThread();
+
 	InitGUIThreads();
 
 	InitProgressThread();
@@ -186,6 +188,8 @@ int MainMenu(int menu)
 	bgImg = new GuiImage(background);
 	mainWindow->Append(bgImg);
 
+	MountGamePartition();
+
 	ResumeGui();
 
 	bgMusic = new GuiBGM(Resources::GetFile("bg_music.ogg"), Resources::GetFileSize("bg_music.ogg"), Settings.volume);
@@ -193,9 +197,7 @@ int MainMenu(int menu)
 	bgMusic->Load(Settings.ogg_path);
 	bgMusic->Play();
 
-	MountGamePartition();
-
-	InitNetworkThread(); // Starting this a little later fixes a random crash
+	WDVD_StopMotor();
 
 	if (Settings.autonetwork)
 		ResumeNetworkThread();
